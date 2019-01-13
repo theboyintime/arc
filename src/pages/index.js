b8763d698fef93af0ce8a0e70f002e654b769d45
Tobi Oluwo <oluwooa@vcu.edu>
2019-01-13 15:03:20 -0500

    testing 1, 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>What the fuck is going on?</p>
+    <h1>Why is this so hard to setup?</h1>
+    <p>Holy shite!</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
